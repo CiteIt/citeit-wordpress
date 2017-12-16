5fca5c4a950d0cb892c303cab45d2f165cf60714
Tim Langeman <timlangeman@gmail.com>
2017-12-16 15:00:39 -0500

    Improve hash generation: escape characters
     * move hash generation into its own function
        for easier testing: quote_hash()
     * modify escape functions to prevent html and text
        version from including character inconsistencies

--- js/neotext-quote-context.js
+++ js/neotext-quote-context.js
@@ -2,8 +2,8 @@
  * Quote-Context JS Library
  * https://github.com/neotext/neotext-quote-context/
  *
- * Copyright 2015-2016, Tim Langeman
- * https://www.openpolitics.com/tim
+ * Copyright 2015-2017, Tim Langeman
+ * http://www.openpolitics.com/tim
  *
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/MIT
@@ -48,11 +48,12 @@ jQuery.fn.quoteContext = function() {
 			// If they have a cite tag, check to see if its hash is already saved
 			if (cited_url.length > 3){
 				var tag_type = jQuery(this)[0].tagName.toLowerCase();
-				var url_quote_text = convert_special_characters(citing_quote) + '|' + trim_encode(citing_url) + '|' + trim_encode(cited_url);
-				var quote_hash = Sha1.hash(url_quote_text);
-				var shard = quote_hash.substring(0,2);
+				var quote_hash_value = quote_hash(citing_quote, citing_url, cited_url)
+				var url_quote_text = escape_quote(citing_quote) + '|' + escape_url(citing_url) + '|' + escape_url(cited_url);
+				var quote_hash_value = Sha1.hash(url_quote_text);
+				var shard = quote_hash_value.substring(0,2);
 				var read_base = 'https://read.neotext.net/quote/';
-				var read_url = read_base.concat("sha1/", version_num, "/", shard, "/", quote_hash, ".json");
+				var read_url = read_base.concat("sha1/", version_num, "/", shard, "/", quote_hash_value, ".json");
 				var json = null;
 
 				//See if a json summary of this quote was already created and uploaded to the content delivery network
@@ -85,21 +86,18 @@ jQuery.fn.quoteContext = function() {
 
 						//Style quote as a link that calls the popup expander:  return this.onclick()
 						blockcite.wrapInner("<a href='" + blockcite.attr('cite') + "' " +
+							//"onmouseover='return this.onclick()' " +
+							//"onmouseover='return this.onclick()'" +
 							"onclick='return expand_popup(this ,\"" + q_id +"\")' " +
 						 " />");
 					}
 					else if ( tag_type == "blockquote"){
 						//Fill 'before' and 'after' divs and then quickly hide them
-						blockcite.before(" \
-							<div id='quote_before_" + json['sha1'] + "' class='quote_context'> \
-							  <blockquote class='quote_context'>.. " + json["cited_context_before"] + "</blockquote> \
-							</div> \
-						");
+						blockcite.before("<div id='quote_before_" + json['sha1'] + "' class='quote_context'> \
+							<blockquote class='quote_context'>.. " + json["cited_context_before"] + "</blockquote></div>");
 
 						blockcite.after("<div id='quote_after_" + json['sha1'] + "' class='quote_context'> \
-							<blockquote class='quote_context'>" + json["cited_context_after"] + " ..</blockquote></div> \
-							<div class='neotext_source'><span class='neotext_source_label'>source: </span> \
-							<a class='neotext_source_domain' href='" + json['cited_url'] + "'>" + extractDomain( json['cited_url'] ) + "</a></div>");
+							<blockquote class='quote_context'>" + json["cited_context_after"] + " ..</blockquote></div>");
 
 						var context_before = jQuery("#quote_before_" + json['sha1']);
 						var context_after = jQuery("#quote_after_" + json['sha1']);
@@ -176,33 +174,81 @@ function close_popup(hidden_popup_id){
 	jQuery(hidden_popup_id).dialog("close");
 }
 
-function trim_encode(str){
-   return str.trim();
+//Source: http://stackoverflow.com/questions/10032024/how-to-remove-leading-and-trailing-white-spaces-from-a-given-html-string
+// Credit:  KhanSharp:  (used for backward compatibility.
+//          trim() introduced in javascript 1.8.1)
+
+function trim_regex(str){
+  return str.replace(/^[ ]+|[ ]+$/g,'')
 }
 
-function convert_special_characters(content) {
-    replace_text = ['\n', '’', ',', '.' , '-', ':', '/', '!', '`', '~', '^',
-		' ', '&nbsp', '\xa0'
-    ]
-    for txt in replace_text{
-        content = content.replace(txt, '')
-	}
-    return content
-} 
+function escape_url(str){
+  var replace_chars_array = [
+		'\n', ' ', '&nbsp',
+  ];
+  str = trim_regex(str); 	// remove whitespace at beginning and end
 
-// Credit: http://stackoverflow.com/questions/8498592/extract-root-domain-name-from-string
-function extractDomain(url) {
-    var domain;
-    //find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
-    }
+  return normalize_text(str, replace_chars_array);
 
-    //find & remove port number
-    domain = domain.split(':')[0];
+}
 
-    return domain;
+function escape_quote(str){
+  var replace_chars_array = ['\n', '’', ',', '.' , '-', ':', '/', '!', '`', '~', '^', 
+		' ', '&nbsp', '\xa0', '&#8217;', 
+		'&#169;', '&copy;', '&#174;', 
+		'&reg;', '&#8364;', '&euro;', '&#8482;', '&trade;', 
+		'&lsquo;', '&rsquo;', '&sbquo;', '&ldquo;', '&rdquo;', '&bdquo;', 
+		'&#34;', '&quot;', '&#38;', '&amp;', '&#39;', '&#163;', '&pound;', 
+		'&#165;', '&yen;', '&#168;', '&uml;', '&die;', '&#169;', '&copy;'
+		];
+
+  return normalize_text(str, replace_chars_array);
+}
+
+// Credit: Sean Bright, MC Emperor
+// https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replace_all(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function normalize_text(str, replace_chars_array){
+  /*	This javascript function performs the same functionality as the python method: 
+		neotext_quote_context.utility.text.normalize()
+
+		It replaces an array of symbols found within the input string
+		with the specified replacement character(s).
+		By default, the replacement_char is an empty string, meaning this function 
+		removes all of replace_chars from the text.	
+ */
+  
+  // Default to empty string
+  replacement_char = '';	
+  
+  // If no array passed, set default
+  if (!(typeof replace_chars_array !== 'undefined' && replace_chars_array.length > 0)) {
+	  var replace_chars_array = [
+			'\n', '’', ',', '.' , '-', ':', '/', '!', '`', '~', '^', 
+			' ', '&nbsp', '\xa0', '&#8217;', '&#169;', '&copy;', '&#174;', 
+			'&reg;', '&#8364;', '&euro;', '&#8482;', '&trade;', 
+			'&lsquo;', '&rsquo;', '&sbquo;', '&ldquo;', '&rdquo;', '&bdquo;', 
+			'&#34;', '&quot;', '&#38;', '&amp;', '&#39;', '&#163;', '&pound;', 
+			'&#165;', '&yen;', '&#168;', '&uml;', '&die;', '&#169;', '&copy;'
+	  ];
+  }
+
+  // loop through replace_chars with "old school" loop, for maximum browser compatibility
+  var index;  
+  for (index = 0; index < replace_chars_array.length; ++index) {
+	str = replace_all(str, replace_chars_array[index], replacement_char); 
+  }
+  return str;
+}
+
+function quote_hash(citing_quote, citing_url, cited_url){
+	var url_quote_text = escape_quote(citing_quote) + '|' + escape_url(citing_url) + '|' + escape_url(cited_url);
+	return Sha1.hash(url_quote_text);
 }
