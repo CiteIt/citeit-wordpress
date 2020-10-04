599a8e0eb788d073fed05810b6033298525d8abc
Tim Langeman <timlangeman@gmail.com>
2020-10-03 23:03:10 -0400

    Revert "Format Blockquote Context & Popup menu"
    This reverts commit ff9822eb9d2f6128f2660e929a6dce405240a8d2.

--- js/versions/0.4/CiteIt-quote-context.js
+++ js/versions/0.4/CiteIt-quote-context.js
@@ -103,8 +103,6 @@ jQuery.fn.quoteContext = function() {
 
                     if (tag_type === "q") {
                         var q_id = "hidden_" + json.sha256;
-                        var url_cited_domain = json.cited_url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
-
 
                         //Add content to a hidden div, so that the popup can later grab it
                         jQuery("#" + hidden_container).append(
@@ -115,7 +113,7 @@ jQuery.fn.quoteContext = function() {
                             "<p><a href='" + json.cited_url +
                             "' target='_blank'>Read more</a> | " +
                             "<a href='javascript:closePopup(" +
-                            q_id + ");'>Close</a> <div class='source_url'>source: <a href='" + json.cited_url + "'>" + url_cited_domain + "</a> </p></div>"
+                            q_id + ");'>Close</a> </p></div>"
                         );
 
                         //Style quote as a link that calls the popup expander:
@@ -127,29 +125,18 @@ jQuery.fn.quoteContext = function() {
                         var embed_ui = embedUi(cited_url, json);
 
                         //Fill 'before' and 'after' divs and then quickly hide them
-                        blockcite.before("<div id='quote_before_" + json.sha256 + "' class='quote_context'>"
-                          + "<blockquote class='quote_context'>"
-                              + "<span class='context_header'>Context Before:</span>"
-                              + "<div class='tooltip'><span class='tooltip_icon'>?</span><span class='tooltiptext'>CiteIt.net displays the 500 characters of Context immediately before and after the quote</span></div><br />" 
-                              + embed_ui.html + " .. " + json.cited_context_before  
-                          + "</blockquote></div>"
-					   );
+                        blockcite.before("<div id='quote_before_" + json.sha256 + "' class='quote_context'> \
+                            <blockquote class='quote_context'>.. <br />" + embed_ui.html + " " + json.cited_context_before + "</blockquote></div> \
+                        ");
 
-                        blockcite.after("<div id='quote_after_" + json.sha256 + "' class='quote_context'>"
-                           + "<blockquote class='quote_context'>"
-                              + ".. " + json.cited_context_after + " .."
-                              + "<br /><span class='context_header'>Context After:</span>" 
-	                          + "<div class='tooltip'><span class='tooltip_icon'>?</span><span class='tooltiptext'>CiteIt.net displays the 500 characters immediately before and after the quote</span></div>" 
-                          + "</blockquote></div>" 
-                          + "<div class='citeit_source'><span class='citeit_source_label'>source: </span>"
-                          + "<a class='citeit_source_domain' href='" + json.cited_url + "'>" + extractDomain(json.cited_url) + "</a></div>"
-                        );
+                        blockcite.after("<div id='quote_after_" + json.sha256 + "' class='quote_context'> \
+                            <blockquote class='quote_context'>" + json.cited_context_after + " ..</blockquote></div> \
+                            <div class='citeit_source'><span class='citeit_source_label'>source: </span> \
+                            <a class='citeit_source_domain' href='" + json.cited_url + "'>" + extractDomain(json.cited_url) + "</a></div> \
+                        ");
 
                         var context_before = jQuery("#quote_before_" + json.sha256);
                         var context_after = jQuery("#quote_after_" + json.sha256);
-
-					   /* Main Class */
-					   blockcite.addClass('quote_text');
 
                         context_before.hide();
                         context_after.hide();
@@ -231,7 +218,7 @@ function expandPopup(tag, hidden_popup_id) {
         resizable: true,
         width: 400,
         modal: false,
-        title: "Quote Context by CiteIt.net",
+        title: "powered by CiteIt.net",
         hide: {
             effect: "size",
             duration: 400
@@ -255,7 +242,7 @@ function expandPopup(tag, hidden_popup_id) {
         effect: "scale",
         duration: 400
     }).dialog({
-        "title": "Quote Context by CiteIt.net"
+        "title": "powered by CiteIt.net"
     }).dialog("open").blur();
 
     // Close popup when you click outside of it
@@ -539,4 +526,5 @@ function isValidUrl(string) {
 
   return url.protocol === "http:" || url.protocol === "https:";
 }
+
 
